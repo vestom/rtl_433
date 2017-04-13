e4c9c393db83d672eb8ab6390035a7415f3e6c34
p1mike <mail@prieskorn.com>
2017-04-13 18:42:59 +0200

    Added normal output with JSON and csv for WS2000
    Works with my own WS2000 but a code review could also help.

--- src/devices/elv.c
+++ src/devices/elv.c
@@ -1,4 +1,8 @@
 #include "rtl_433.h"
+#include "data.h"
+#include "util.h"
+#include <math.h>
+
 
 uint16_t AD_POP(uint8_t bb[BITBUF_COLS], uint8_t bits, uint8_t bit) {
     uint16_t val = 0;
@@ -64,43 +68,111 @@ static int em1000_callback(bitbuffer_t *bitbuffer) {
 
     return 1;
 }
+float get_ws2000_humidity(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included to support sensors with humidity in different position
+    float humidity = 0;
+    humidity = message[7]*100 + message[6]*10 + message[5];
+    humidity = humidity / 10;
+    return humidity;
+}
+float get_ws2000_temperature(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included  to support sensors with temp in different position
+    float temp_c = 0;
+    temp_c = (((message[4])*100)+ message[3]*10 + message[2]);
+    temp_c = temp_c / 10;
+    if (message[1] & 0x08)
+        temp_c = -temp_c;
+    return temp_c;
+}
+unsigned int get_ws2000_pressure(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included to support sensors with humidity in different position
+    unsigned int pressure = 0;
+    pressure = 200 + message[10]*100 + message[9]*10 + message[8];
+    return pressure;
+}
+unsigned int get_ws2000_rain_sum(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included to support sensors with humidity in different position
+    unsigned int rain_sum = 0;
+    rain_sum = message[4]*16*16 + message[3]*16 + message[2];
+    return rain_sum;
+}
+unsigned int get_ws2000_luminance(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included to support sensors with humidity in different position
+    unsigned int luminance = 0;
+    luminance = (message[4]*100 + message[3]*10 + message[2])* powl(10, message[5]);
+    return luminance;
+}
+unsigned int get_ws2000_sunminutes(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included to support sensors with humidity in different position
+    unsigned int sun_minutes = 0;
+    sun_minutes = message[8]*16*16 + message[7]*16 + message[6];
+    return sun_minutes;
+}
+float get_ws2000_windspeed(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included  to support sensors with temp in different position
+    float wind = 0;
+    wind = (float) message[2]/10 + message[4]*10 + message[3];
+    if (message[1] & 0x08)
+        wind += 100;
+    return wind;
+}
+unsigned int get_ws2000_direction(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included to support sensors with humidity in different position
+    unsigned int direction = 0;
+    direction = (message[7]&3)*100 + message[6]*10 + message[5];
+    return direction;
+}
+float get_ws2000_variation(unsigned char *message, unsigned int sensor_id) {
+    // sensor ID included  to support sensors with temp in different position
+    float variation = 0;
+    variation = (float) (message[7]>>2) * 22.5;
+    return variation;
+}
+
+
 
 static int ws2000_callback(bitbuffer_t *bitbuffer) {
     // based on http://www.dc3yc.privat.t-online.de/protocol.htm
+    // and on (Anemometer & luminance) http://www.f6fbb.org/domo/sensors/
     bitrow_t *bb = bitbuffer->bb;
     uint8_t dec[13];
     uint8_t nibbles=0;
     uint8_t bit=11; // preamble
     char* types[]={"!AS3", "AS2000/ASH2000/S2000/S2001A/S2001IA/ASH2200/S300IA", "!S2000R", "!S2000W", "S2001I/S2001ID", "!S2500H", "!Pyrano", "!KS200/KS300"};
+    uint8_t length[16]={5, 8, 5, 8, 12, 9, 8, 14};
     uint8_t check_calculated=0, sum_calculated=0;
     uint8_t i;
     uint8_t stopbit;
 	uint8_t sum_received;
+    data_t *data;
+    char time_str[LOCAL_TIME_BUFLEN];
+    local_time_str(0, time_str);
 
     dec[0] = AD_POP (bb[0], 4, bit); bit+=4;
     stopbit= AD_POP (bb[0], 1, bit); bit+=1;
     if (!stopbit) {
-//fprintf(stdout, "!stopbit\n");
+        if(debug_output) fprintf(stdout, "!stopbit\n");
         return 0;
     }
     check_calculated ^= dec[0];
     sum_calculated   += dec[0];
 
     // read nibbles with stopbit ...
-    for (i = 1; i <= (dec[0]==4?12:8); i++) {
+    for (i = 1; i <= length[dec[0]]; i++) {
         dec[i] = AD_POP (bb[0], 4, bit); bit+=4;
         stopbit= AD_POP (bb[0], 1, bit); bit+=1;
         if (!stopbit) {
-//fprintf(stdout, "!stopbit %i\n", i);
+            if(debug_output) fprintf(stdout, "!stopbit %i\n", bit);
             return 0;
         }
         check_calculated ^= dec[i];
         sum_calculated   += dec[i];
         nibbles++;
     }
+    if(debug_output) { for (i = 0; i < nibbles; i++) fprintf(stdout, "%02X ", dec[i]); fprintf(stdout, "\n"); }
 
     if (check_calculated) {
-//fprintf(stdout, "check_calculated (%d) != 0\n", check_calculated);
+        if(debug_output) fprintf(stdout, "check_calculated (%d) != 0\n", check_calculated);
         return 0;
     }
 
@@ -109,24 +181,128 @@ static int ws2000_callback(bitbuffer_t *bitbuffer) {
     sum_calculated+=5;
     sum_calculated&=0xF;
     if (sum_received != sum_calculated) {
-//fprintf(stdout, "sum_received (%d) != sum_calculated (%d) ", sum_received, sum_calculated);
+        if(debug_output) fprintf(stdout, "sum_received (%d) != sum_calculated (%d) ", sum_received, sum_calculated);
         return 0;
     }
+    if (dec[0]==0 || dec[0]==1 || dec[0]==4)
+    {
+        float humidity = 0;
+        if (dec[0]>0)
+            humidity = get_ws2000_humidity(dec, 1);
+        if (humidity==0)
+        {
+            data = data_make(
+                             "time",          "",            DATA_STRING, time_str,
+                             "brand",         "Station",     DATA_STRING, "WS2000",
+                             "model",         "Sensor",      DATA_STRING, "Temperature",
+                             "address",       "Address",     DATA_INT,    (dec[1] & 7),
+                             "temperature_C", "Celcius",     DATA_FORMAT, "%.1f C", DATA_DOUBLE, get_ws2000_temperature(dec, 1), // set 1 instead sensor_id
+                             NULL);
+            data_acquired_handler(data);
+        }
+        if (humidity>0.0 && dec[0] != 4)
+        {
+            data = data_make(
+                             "time",          "",           DATA_STRING, time_str,
+                             "brand",         "Station",    DATA_STRING, "WS2000",
+                             "model",         "Sensor",     DATA_STRING, "Temperature/Humidity",
+                             "address",       "Address",    DATA_INT,    (dec[1] & 7),
+                             "temperature_C", "Celcius",    DATA_FORMAT, "%.1f C", DATA_DOUBLE, get_ws2000_temperature(dec, 1), // set 1 instead sensor_id
+                             "humidity",      "Humidity",   DATA_FORMAT, "%.1f %%", DATA_DOUBLE, humidity,
+                             NULL);
+            data_acquired_handler(data);
+        }
+        if (humidity>0 && dec[0] == 4)
+        {
+            data = data_make(
+                             "time",           "",           DATA_STRING, time_str,
+                             "brand",          "Station",    DATA_STRING, "WS2000",
+                             "model",          "Sensor",     DATA_STRING, "Temperature/Humidity/Pressure",
+                             "address",        "Address",    DATA_INT,    (dec[1] & 7),
+                             "temperature_C",  "Celcius",    DATA_FORMAT, "%.1f C", DATA_DOUBLE, get_ws2000_temperature(dec, 1), // set 1 instead sensor_id
+                             "humidity",       "Humidity",   DATA_FORMAT, "%.1f %%", DATA_DOUBLE, humidity,
+                             "pressure",       "Pressure",   DATA_FORMAT, "%u hPa", DATA_INT,get_ws2000_pressure(dec, 1),
+                             NULL);
+            data_acquired_handler(data);
+        }
 
-//for (i = 0; i < nibbles; i++) fprintf(stdout, "%02X ", dec[i]); fprintf(stdout, "\n");
-
-    fprintf(stdout, "Weather station sensor event:\n");
-    fprintf(stdout, "protocol      = ELV WS 2000, %d bits\n",bitbuffer->bits_per_row[1]);
-    fprintf(stdout, "type (!=ToDo) = %s\n", dec[0]<=7?types[dec[0]]:"?");
-    fprintf(stdout, "code          = %d\n", dec[1]&7);
-    fprintf(stdout, "temp          = %s%d.%d\n", dec[1]&8?"-":"", dec[4]*10+dec[3], dec[2]);
-    fprintf(stdout, "humidity      = %d.%d\n", dec[7]*10+dec[6], dec[5]);
-    if(dec[0]==4) {
-        fprintf(stdout, "pressure      = %d\n", 200+dec[10]*100+dec[9]*10+dec[8]);
     }
+    else if (dec[0]==2)   // rain
+        {  data = data_make(
+                         "time",           "",           DATA_STRING, time_str,
+                         "brand",          "Station",    DATA_STRING, "WS2000",
+                         "model",          "Sensor",     DATA_STRING, "Rain",
+                         "address",        "Address",    DATA_INT,    (dec[1] & 7),
+                         "rain_sum",       "Rain Counter",           DATA_INT,get_ws2000_rain_sum(dec, 1), // Counter for rain gauge impulses; reset to 0 after 4095
+                         NULL);
+            data_acquired_handler(data);
+        
+
+    }
+    else if (dec[0]==3)   // wind
+    {
+        data = data_make(
+                         "time",           "",           DATA_STRING, time_str,
+                         "brand",          "Station",    DATA_STRING, "WS2000",
+                         "model",          "Sensor",     DATA_STRING, "Wind",
+                         "address",        "Address",    DATA_INT,    (dec[1] & 7),
+                         "wind_speed",     "Wind",       DATA_FORMAT, "%.1f km/h", DATA_DOUBLE, get_ws2000_windspeed(dec, 1), // set 1 instead sensor_id
+                         "direction",      "Direction",  DATA_FORMAT, "%u deg", DATA_INT, get_ws2000_direction(dec, 1),
+                         "variation",      "Variation",  DATA_FORMAT, "%.01f deg", DATA_DOUBLE, get_ws2000_variation(dec, 1),
+                         NULL);
+        data_acquired_handler(data);
+
+
+    } else if (dec[0]==5)   // luminance
+    {
+        data = data_make(
+                         "time",           "",            DATA_STRING, time_str,
+                         "brand",          "Station",     DATA_STRING, "WS2000",
+                         "model",          "Sensor",      DATA_STRING, "Luminance",
+                         "address",        "Address",     DATA_INT,    (dec[1] & 7),
+                         "luminance",      "Luminance",   DATA_INT,get_ws2000_luminance(dec, 1),
+                         "sun_minutes",    "Sun minutes", DATA_INT,get_ws2000_sunminutes(dec, 1), // Counter for minutes >20kLux reset to 0 after 4095
+                         NULL);
+        data_acquired_handler(data);
+        
+    } else if (dec[0]==6)   // pyrano (ToDo)
+    {
+    } else if (dec[0]==7)   // kombi (ToDo)
+    {
+    }
+
+    //fprintf(stdout, "Weather station sensor event:\n");
+    //fprintf(stdout, "protocol      = ELV WS 2000, %d bits\n",bitbuffer->bits_per_row[1]);
+    //fprintf(stdout, "type (!=ToDo) = %s\n", dec[0]<=7?types[dec[0]]:"?");
+    //fprintf(stdout, "code          = %d\n", dec[1]&7);
+    //fprintf(stdout, "temp          = %s%d.%d\n", dec[1]&8?"-":"", dec[4]*10+dec[3], dec[2]);
+    //fprintf(stdout, "humidity      = %d.%d\n", dec[7]*10+dec[6], dec[5]);
+    //if(dec[0]==4) {
+    //    fprintf(stdout, "pressure      = %d\n", 200+dec[10]*100+dec[9]*10+dec[8]);
+    //}
 
     return 1;
 }
+/*
+ * List of fields to output when using CSV
+ *
+ * Used to determine what fields will be output in what
+ * order for this devince when using -F csv.
+ *
+ */
+static char *csv_output_fields[] = {
+    "time",
+    "model",
+    "id",
+    "temperature_C",
+    "humidity",
+    "pressure",
+    "rain_sum",
+    "luminance",
+    "wind_speed",
+    "direction",
+    NULL
+};
 
 r_device elv_em1000 = {
     .name           = "ELV EM 1000",
@@ -141,11 +317,12 @@ r_device elv_em1000 = {
 
 r_device elv_ws2000 = {
     .name           = "ELV WS 2000",
-    .modulation     = OOK_PULSE_PPM_RAW,
-    .short_limit    = 602+(1155-602)/2,
-    .long_limit     = (1755635-1655517)/2, // no repetitions
-    .reset_limit    = (1755635-1655517)*2,
+    .modulation     = OOK_PULSE_PWM_RAW,
+    .short_limit    = (854+366)/2,  // 0 => 854us, 1 => 366us according to link in top
+    .long_limit     = 1000, // no repetitions
+    .reset_limit    = 1000, // Longest pause is 854us according to link
     .json_callback  = &ws2000_callback,
-    .disabled       = 1,
+    .fields         = csv_output_fields,
+    .disabled       = 0,
     .demod_arg      = 0,
 };
